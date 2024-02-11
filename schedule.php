91843c49760605fbc7c93bf06a794a82472bf8a9
Vinz Rulloda <102574744+DaVinzci@users.noreply.github.com>
2024-02-11 21:06:49 +0800

    test

--- schedule.php
+++ schedule.php
@@ -1,5 +1,6 @@
 <?php
 require 'db_connection.php';
+session_start();
 
 $stmt = $conn->prepare("INSERT INTO schedule (start_time, end_time, schedule_name) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $start_time, $end_time, $schedule_name);
@@ -8,6 +9,16 @@ $start_time = $_POST['start_time'];
 $end_time = $_POST['end_time'];
 $schedule_name = $_POST['schedule_name'];
 $stmt->execute();
+
+
+$stmt = $conn->prepare("INSERT INTO history (`event`, user_id) VALUES (?, ?)");
+$stmt->bind_param("ss", $event, $user_id);
+
+$event = "Added new schedule.";
+$user_id = $_SESSION['uid'];
+$stmt->execute();
+
+
 
 Header('Location: manager.php');
 ?>
